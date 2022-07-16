c5b6631b59921bf8f2f268b464c98094abebdafb
Shaik Jaffar <107207838+skjaffar@users.noreply.github.com>
2022-07-16 22:26:03 +0530

    Add files via upload

---
+++ practice programs.py
@@ -0,0 +1,55 @@
+# id=input("enter the id number:")
+# name=input("enter your name:")
+# company=input("enter company name:")
+# salary=float(input("enter the salary:"))
+# print("your id number is: {},your name is:{},working company name is:{}and salary is:{}".format(id,name,company,salary))
+
+
+# """ sum and average:"""
+
+
+# a,b,c=[float(x) for x in input("enter three values ").split(",")]
+# sum =a+b+c
+# average=sum/3
+# print("the sum is%.2f\naverage is %.2f " %(sum,average))
+       
+       
+    # """   area of circle:"""
+    
+    
+# r=float(input("enter the radious:")) 
+# PI=22/7
+# area= PI*r*r   
+# print("area of a circle is: %.2f "  %area)
+           
+      
+      
+        # """    cube of number: """
+  
+  
+# n=float(input("enter the any number:"))
+# print("cube of any number  ",n**3)           
+ 
+ 
+#  """"even and odd number:"""
+
+
+# n=int(input("enter the value of n:"))
+# if n%2==0:
+#     print("enter the even number")
+# else:
+#     print("enter number is odd ")    
+ 
+ 
+#  """"get charecter on display:''''
+
+# n=input("enter the charecter:")
+# print("the entered charecter is: " +n[0])
+# print("the entered charecter is: " ,n[0])
+
+
+# ""'" joining two strings ""''
+
+# s1=input("enter the two strings:")
+# s2=input("enter the two string:")
+# print(s1 + s2)
